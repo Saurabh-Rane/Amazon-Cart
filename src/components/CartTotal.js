b34e7695a9ec456eadbf9ff9aeaae2cac0477104
Saurabh Rane <ssrane@asu.edu>
2021-10-12 12:36:41 -0700

    Format total price value

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CartTotal.css';
+import NumberFormat from 'react-number-format';
 
 function CartTotal({ items }) {
 	const getTotalPrice = () => {
@@ -10,11 +11,27 @@ function CartTotal({ items }) {
 		return total;
 	};
 
+	const getCartItems = () => {
+		let total = 0;
+		items.forEach((item) => {
+			total += item.quantity;
+		});
+		return total;
+	};
+
 	return (
 		<div className="CartTotal">
 			<h3>
-				Subtotal({items.length} items):
-				<span className="CartTotal-price">${getTotalPrice()}</span>
+				Subtotal({getCartItems()} items):
+				<span className="CartTotal-price">
+					<NumberFormat
+						value={getTotalPrice()}
+						decimalScale={2}
+						displayType={'text'}
+						thousandSeparator={true}
+						prefix={'$'}
+					/>
+				</span>
 			</h3>
 			<button>Proceed to checkout</button>
 		</div>
